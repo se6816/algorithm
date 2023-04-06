9e8e945bee822e588d827c11e7505e8457ae1d4c
se6816 <se6816@naver.com>
2023-04-06 11:33:44 +0900

    BJ 2096 내려가기

--- src/BJ2096.java
+++ src/BJ2096.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BJ2096 {
 	
 
 	public static void main(String[] args) throws IOException{
