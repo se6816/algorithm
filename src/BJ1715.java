e90d1fbf22268baed1c82ac1d34916596251505a
se6816 <se6816@naver.com>
2023-05-12 08:44:19 +0900

    BJ 1715 카드 정렬하기

--- src/BJ1715.java
+++ src/BJ1715.java
@@ -13,15 +13,12 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 /**
- * 해당 문제는 위상 정렬문제이다. 
- * 그러나 주의할 점이 있다. 연속적으로 정렬이 이루어지면서 하는 것이 아닌
- * 1번째 턴에 선수 아이템 없는 것들끼리 모아서 정렬하여 처리하고
- * 처리가 다 끝나고 2번 째 턴에 선수 아이템이 없는 것들끼리 모아서 처리하는 것을 반복해야 한다. 
+ * 해당 문제는 단순한 그리디 문제이다. 
  * 
  * @author user
  *
  */
-public class Main{
+public class BJ1715{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PriorityQueue<Integer> pq=new PriorityQueue<>();
